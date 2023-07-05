55de9b4ae1fc5f46e0e190fe7c287a871bf3acc4
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-07-04 21:28:23 -0300

    se agrega el controlador para eliminar y editar una receta buscando por ID

--- src/controllers/recetas.controllers.js
+++ src/controllers/recetas.controllers.js
@@ -42,3 +42,31 @@ export const obtenerReceta = async (req, res) => {
         })
     }
 }
+
+export const borrarReceta = async (req, res) => {
+    try{
+        await Receta.findByIdAndDelete(req.params.id);
+        res.status(200).json({
+            mensaje: "La receta fue eliminada correctamente"
+           })
+    }catch(error){
+        console.log(error)
+        res.status(404).json({
+            mensaje: "Error. No se pudo borrar la receta."
+        })
+    }
+}
+
+export const editarReceta = async (req, res) =>{
+    try{
+        await Receta.findByIdAndUpdate(req.params.id, req.body);
+        res.status(200).json({
+            mensaje: "La receta fue editada correctamente"
+           })
+    }catch(error){
+        console.log(error)
+        res.status(404).json({
+            mensaje: "Error. No se pudo editar la receta."
+        })
+    }
+}
--- src/routes/recetas.routes.js
+++ src/routes/recetas.routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { controlPrueba, crearReceta, obtenerListaRecetas, obtenerReceta } from "../controllers/recetas.controllers";
+import { borrarReceta, controlPrueba, crearReceta, editarReceta, obtenerListaRecetas, obtenerReceta } from "../controllers/recetas.controllers";
 
 const router = Router();
 router.route("/prueba").get(controlPrueba)
 router.route("/recetas").post(crearReceta).get(obtenerListaRecetas)
-router.route("/recetas/:id").get(obtenerReceta)
+router.route("/recetas/:id").get(obtenerReceta).delete(borrarReceta).put(editarReceta)
 
 export default router;
